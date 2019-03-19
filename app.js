8331f9af5305f6a51ab0597059952e01e25ebb4f
Matt Davis <matthewpeterdavis@gmail.com>
2019-03-19 09:59:06 +0200

    tidy

--- app.js
+++ app.js
@@ -2,31 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.get("/", function(req, res) {
-  res.sendFile("Welcome to our brand server");
+  res.send("Welcome to our brand server");
 });
 
 const brands = {
-  Nike: { score: 83 },
-  Nicce: { score: 98 },
-  Carhartt: { score: 45 }
+  nike: { score: 83 },
+  nicce: { score: 98 },
+  carhartt: { score: 45 }
 };
 
 app.get("/brand", (req, res, next) => {
   const { brand } = req.query;
 
-  return res.json(brands[brand]);
+  return res.json(brands[brand.toLowerCase()]);
 });
 
 module.exports = app;
