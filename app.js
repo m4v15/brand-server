d54f58ba0187b6cfca5189e35ca4b65eed378825
Matt Davis <matthewpeterdavis@gmail.com>
2019-03-19 14:25:41 +0200

    add colour hex to response

--- app.js
+++ app.js
@@ -9,13 +9,34 @@ app.get("/", function(req, res) {
 const brands = {
   nike: { score: 83 },
   nicce: { score: 98 },
-  carhartt: { score: 45 }
+  carhartt: { score: 45 },
+  adidas: { score: 69 },
+  "pull&bear": { score: 55 },
+  collusion: { score: 75 },
+  celio: { score: 66 }
 };
+
+const colourKey = {
+  50: "FF0000",
+  60: "FF0000",
+  70: "FFFF00",
+  80: "FFFF00",
+  90: "ADFF2F",
+  100: "ADFF2F"
+};
+
+function roundUp10(num) {
+  return Math.ceil(num / 10) * 10;
+}
 
 app.get("/brand", (req, res, next) => {
   const { brand } = req.query;
+  const brandData = brands[brand.toLowerCase()];
+  const score = brandData.score;
+  const rounded = roundUp10(score);
+  const colourHex = colourKey[rounded];
 
-  return res.json(brands[brand.toLowerCase()]);
+  return res.json({ score, colourHex });
 });
 
 module.exports = app;
